0d13a733bfd3762c0900ea25cf489cdfb8dca65d
NitshianecGo <tanz.devil.nitshianec@gmail.com>
2026-08-11 20:53:47 +0900

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,12 +3,13 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-comp
 
 // Вставьте здесь ваши данные Firebase Config
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+ apiKey: "AIzaSyBRi7lwyM1XELz02Gy_llBXt3c0V7kpLCI",
+  authDomain: "homa-27efb.firebaseapp.com",
+  databaseURL: "https://homa-27efb-default-rtdb.firebaseio.com",
+  projectId: "homa-27efb",
+  storageBucket: "homa-27efb.firebasestorage.app",
+  messagingSenderId: "365610803694",
+  appId: "1:365610803694:web:76a5554f8ab0c51c0f2eff"
 });
 
 const messaging = firebase.messaging();
